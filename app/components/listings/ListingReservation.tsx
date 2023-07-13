072dadc9d42d6ff9ee8dc74de0fd89a57242bc99
Nithin Raj <nithinraj9100@gmail.com>
2023-07-13 22:49:00 +0530

    made price in sync

--- app/components/listings/ListingReservation.tsx
+++ app/components/listings/ListingReservation.tsx
@@ -26,7 +26,7 @@ const ListingReservation: React.FC<ListingReservationProps> = ({
 	return (
 		<div className='overflow-hidden rounded-xl border border-neutral-300 bg-white'>
 			<div className='flex flex-row items-center gap-1 p-4'>
-				<div className='text-2xl font-semibold'>$ {price}</div>
+				<div className='text-2xl font-semibold'>$ {totalPrice}</div>
 				<div className='font-light text-neutral-600'>per night</div>
 			</div>
 			<hr />

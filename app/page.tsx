4a7dbbca613008f5a4e80f8f4b02096838f60129
Nithin Raj <nithinraj9100@gmail.com>
2023-07-13 22:49:57 +0530

    added missing key attribute

--- app/page.tsx
+++ app/page.tsx
@@ -23,13 +23,13 @@ export default async function Home() {
 				<div className='grid grid-cols-1 gap-8 pt-24 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6'>
 					<>
 						{listings?.map(listing => (
-							<>
+							<div key={listing.id}>
 								<ListingCard
 									key={listing.id}
 									data={listing}
 									currentUser={currentUser}
 								/>
-							</>
+							</div>
 						))}
 					</>
 				</div>

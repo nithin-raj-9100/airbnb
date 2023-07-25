6bcc68d26e84ce9e8d5ac5e9c96e0499014b3a05
vamshi <mvamshikrishnareddy469@gmail.com>
2023-07-25 20:20:32 +0530

    test pull request

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,41 +1,44 @@
-import { Analytics } from '@vercel/analytics/react';
-import { Nunito } from 'next/font/google';
+import { Analytics } from "@vercel/analytics/react";
+import { Nunito } from "next/font/google";
 
 // [ ]internal importss
-import ToasterProvider from './providers/ToasterProvider';
-import RegisterModal from './components/models/RegisterModal';
-import Navbar from './components/navbar/Navbar';
-import './globals.css';
-import LoginModal from './components/models/LoginModal';
-import getUser from './actions/getUser';
-import RentModal from './components/models/RentModal';
+import ToasterProvider from "./providers/ToasterProvider";
+import RegisterModal from "./components/models/RegisterModal";
+import Navbar from "./components/navbar/Navbar";
+import "./globals.css";
+import LoginModal from "./components/models/LoginModal";
+import getUser from "./actions/getUser";
+import RentModal from "./components/models/RentModal";
 
 export const metadata = {
-	title: 'Holiday Homes & Apartment Rentals - Airbnb - Airbnb',
-	description: 'airbnb',
+  title: "Holiday Homes & Apartment Rentals - Airbnb - Airbnb",
+  description: "airbnb",
 };
 
 const fonts: any = Nunito({
-	subsets: ['latin'],
+  subsets: ["latin"],
 });
 
 export default async function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	const user = await getUser();
-	return (
-		<html lang='en'>
-			<body className={fonts.className}>
-				<ToasterProvider />
-				<Analytics />
-				<RentModal />
-				<RegisterModal />
-				<LoginModal />
-				<Navbar user={user} />
-				<div className='pb-20 pt-28'>{children}</div>
-			</body>
-		</html>
-	);
+  const user = await getUser();
+
+  console.log("this is the entrance for my nextjs app router");
+
+  return (
+    <html lang="en">
+      <body className={fonts.className}>
+        <ToasterProvider />
+        <Analytics />
+        <RentModal />
+        <RegisterModal />
+        <LoginModal />
+        <Navbar user={user} />
+        <div className="pb-20 pt-28">{children}</div>
+      </body>
+    </html>
+  );
 }
